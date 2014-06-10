f415108ed31977cb41ec4543e6a39b125ac57476
Bruno Azevedo <azevedo.252@gmail.com>
2014-06-10 10:33:06 +0100

    Query and parse the tvdb API

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+var _ = require('lodash');
 
 var showSchema = new mongoose.Schema({
   _id: Number,
@@ -90,6 +94,93 @@ app.get('/api/shows/:id', function(req, res, next) {
   Show.findById(req.params.id, function(err, show) {
     if (err) return next(err);
     res.send(show);
+  });
+});
+
+app.post('/api/shows', function(req, res, next) {
+  var apiKey = '2C3181D199F38572';
+  var parser = xml2js.Parser({
+    // disables conversion to arrays when there is only one child element
+    explicitArray: false,
+    // Normalizes all tags to lowercase
+    normalizeTags: true
+  });
+  var seriesName = req.body.showName
+    .toLowerCase()
+    .replace(/ /g, '_')
+    .replace(/[^\w-]+/g, '');
+
+  async.waterfall([
+    // Get the Show ID given the Show Name
+    function(callback) {
+      request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          if (!result.data.series) {
+            return res.send(404, { message: req.body.showName + ' was not found.' });
+          }
+          var seriesId = result.data.series.seriesid || result.data.series[0].seriesid;
+          callback(err, seriesId);
+        });
+      });
+    },
+    // Get the show information using the Show ID from previous step
+    function(seriesId, callback) {
+      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          var series = result.data.series;
+          var episodes = result.data.episode;
+          var show = new Show({
+            _id: series.id,
+            name: series.seriesname,
+            airsDayOfWeek: series.airs_dayofweek,
+            airsTime: series.airs_time,
+            firstAired: series.firstaired,
+            genre: series.genre.split('|').filter(Boolean),
+            network: series.network,
+            overview: series.overview,
+            rating: series.rating,
+            ratingCount: series.ratingcount,
+            runtime: series.runtime,
+            status: series.status,
+            poster: series.poster,
+            episodes: []
+          });
+          _.each(episodes, function(episode) {
+            show.episodes.push({
+              season: episode.seasonnumber,
+              episodeNumber: episode.episodenumber,
+              episodeName: episode.episodename,
+              firstAired: episode.firstaired,
+              overview: episode.overview
+            });
+          });
+          callback(err, show);
+        });
+      });
+    },
+    // Convert the poster image to Base64, assign it to show.poster
+    function(show, callback) {
+      var url = 'http://thetvdb.com/banners/' + show.poster;
+      request({ url: url, encoding: null }, function(error, response, body) {
+        show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+        callback(error, show);
+      });
+    }
+  ], function(err, show) {
+    if (err) return next(err);
+    // Save the Show object to the database
+    show.save(function(err) {
+      if (err) {
+        // duplicate key error
+        if (err.code == 11000) {
+          return res.send(409, { message: show.name + ' already exists.' });
+        }
+        return next(err);
+      }
+      res.send(200);
+    });
   });
 });
 
