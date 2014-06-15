eaffdd6123b51af4a1b30942e4778a6280e8808d
Bruno Azevedo <azevedo.252@gmail.com>
2014-06-15 09:48:47 +0100

    Server side authentication

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 var _ = require('lodash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+// ********************* Mongodb schemas *********************
 
 var showSchema = new mongoose.Schema({
   _id: Number,
@@ -65,6 +71,38 @@ var Show = mongoose.model('Show', showSchema);
 
 mongoose.connect('localhost');
 
+
+// ********************* Passport configuration *********************
+
+// Serialize and deserialize methods are used to keep you signed-in
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  function(email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      user.comparePassword(password, function(err, isMatch) {
+        if (err) return done(err);
+        if (isMatch) return done(null, user);
+        return done(null, false);
+      });
+    });
+  }
+));
+
+
+// ********************* Express configuration *********************
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -72,9 +110,41 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Express API Routes
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.send(401);
+}
+
+// Authentication API Routes
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  // TODO find better solution than to send user’s password over the network and to store it in a cookie
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
+
+// TODO make input validation
+app.post('/api/signup', function(req, res, next) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err) {
+    if (err) return next(err);
+    res.send(200);
+  });
+});
+
+app.get('/api/logout', function(req, res, next) {
+  req.logout();
+  res.send(200);
+});
+
+// Shows API Routes
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
   if (req.query.genre) {
@@ -192,6 +262,13 @@ app.get('*', function(req, res) {
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.send(500, { message: err.message });
+});
+
+app.use(function(req, res, next) {
+  if (req.user) {
+    res.cookie('user', JSON.stringify(req.user));
+  }
+  next();
 });
 
 app.listen(app.get('port'), function() {
